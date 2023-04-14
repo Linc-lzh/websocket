e30f0d935644b7893df2f7694af0cf70fb2dc8d2
Linc_li <linc_li@sina.cn>
2023-04-13 17:55:03 -0700

    add websocket demo code

---
+++ Spring-websocket/src/main/java/boot/spring/service/impl/LoginServiceImpl.java
@@ -0,0 +1,50 @@
+package boot.spring.service.impl;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import boot.spring.mapper.LoginMapper;
+import boot.spring.po.Staff;
+import boot.spring.service.LoginService;
+
+@Transactional(propagation=Propagation.REQUIRED,isolation=Isolation.DEFAULT,timeout=5)
+@Service("loginservice")
+public class LoginServiceImpl implements LoginService{
+	@Autowired
+	private LoginMapper loginmapper;
+	
+	@Override
+	public String getpwdbyname(String name) {
+		Staff s=loginmapper.getpwdbyname(name);
+		if(s!=null) {
+			return s.getPassword();
+		}
+		return null;
+	}
+
+	@Override
+	public Long getUidbyname(String name) {
+		Staff s = loginmapper.getpwdbyname(name);
+		if (s != null){
+			return (long) s.getStaff_id();
+		}
+		return null;
+	}
+
+	@Override
+	public String getnamebyid(long id) {
+		Staff s=loginmapper.getnamebyid(id);
+		if(s!=null) {
+			return s.getUsername();
+		}
+		return null;
+	}
+	
+	
+
+}
---
+++ websocket-demo2/server.js
@@ -0,0 +1,37 @@
+
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
+
+app.listen(80);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+ 
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+  
+  // socket.io 使用 emit(eventname,data) 发送消息，使用on(eventname,callback)监听消息
+     //监听客户端发送的 sendMsg 事件
+	 socket.on("sendMsg", function (data) {
+		 // data 为客户端发送的消息，可以是 字符串，json对象或buffer
+		 // 使用 emit 发送消息，broadcast 表示 除自己以外的所有已连接的socket客户端。
+		 socket.broadcast.emit("receiveMsg", data);
+	 })
+  
+});
+
